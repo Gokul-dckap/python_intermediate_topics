2a49d1bc34bb668cfcc1f02dfa4a6b9d351ac39e
dhanushdckap <dhanushgdckap@gmail.com>
2024-07-16 14:40:38 +0530

    map funcation with the lambda

--- map.py
+++ map.py
@@ -28,3 +28,20 @@
 # numbers = [1, 2, 3, 4, 5]
 # result = list(map(double_even, numbers))
 # print(result)
+
+# || -------------------------------------------------------- ||
+
+# # Double all numbers using map and lambda
+#
+# numbers = (1, 2, 3, 4)
+# result = map(lambda x: x + x, numbers)
+# print(list(result))
+
+
+# # Add two lists using map and lambda
+#
+# numbers1 = [1, 2, 3]
+# numbers2 = [4, 5, 6]
+#
+# result = map(lambda x, y: x + y, numbers1, numbers2)
+# print(list(result))
